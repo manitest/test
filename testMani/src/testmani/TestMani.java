19999affc5bc3d89dc23e7a6ab9edd50caf108b7
gllera <gllera@ubuntu>
2012-01-26 06:50:03 +0100

    Primero

--- testMani/src/testmani/TestMani.java
+++ testMani/src/testmani/TestMani.java
@@ -14,6 +14,6 @@ public class TestMani {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // TODO code application
     }
 }
